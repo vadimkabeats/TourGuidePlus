f6426dd5529f6c1d24f0d8ee7c15b2d5ef19856e
Vadim-Li-V <vadker4@gmail.com>
2025-07-01 22:30:42 +0700

    Добавил Screen.kt для навигации

---
+++ app/src/main/java/com/example/tourguideplus/navigation/Screen.kt
@@ -0,0 +1,9 @@
+package com.example.tourguideplus.navigation
+
+sealed class Screen(val route: String, val title: String) {
+    object Places : Screen("places", "Места")
+    object Routes : Screen("routes", "Маршруты")
+    object Favorites : Screen("favorites", "Избранное")
+    object Weather : Screen("weather", "Погода")
+    object Help : Screen("help", "Справка")
+}
