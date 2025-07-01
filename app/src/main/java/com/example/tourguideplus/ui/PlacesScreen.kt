f4eba56737f632ed8d6196542591cde0293c4117
Vadim-Li-V <vadker4@gmail.com>
2025-07-01 22:33:01 +0700

    Добавил Заглушку экрана PlacesScreen.kt

---
+++ app/src/main/java/com/example/tourguideplus/ui/PlacesScreen.kt
@@ -0,0 +1,12 @@
+package com.example.tourguideplus.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+
+@Composable
+fun PlacesScreen() {
+    Text(text = "Экран Места", modifier = Modifier.padding(16.dp))
+}
