43c3ad2fb209f6d2ae4e915fbb79ed4605e52f73
Vadim-Li-V <vadker4@gmail.com>
2025-07-01 22:34:27 +0700

    Добавил Заглушку экрана FavouritesScreen.kt

---
+++ app/src/main/java/com/example/tourguideplus/ui/FavoritesScreen.kt
@@ -0,0 +1,12 @@
+package com.example.tourguideplus.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+
+@Composable
+fun FavoritesScreen() {
+    Text(text = "Экран Избранное", modifier = Modifier.padding(16.dp))
+}
