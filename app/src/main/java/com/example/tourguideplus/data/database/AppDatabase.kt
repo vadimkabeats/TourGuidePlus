261363d6dd08f65f9754443ec28e267778b5df7e
Vadim-Li-V <vadker4@gmail.com>
2025-07-01 23:08:13 +0700

    Синглтон базы

---
+++ app/src/main/java/com/example/tourguideplus/data/database/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.tourguideplus.data.database
+
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.tourguideplus.data.dao.PlaceDao
+import com.example.tourguideplus.data.model.Place
+
+@Database(
+    entities = [Place::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun placeDao(): PlaceDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                val inst = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "tourguide_db"
+                ).build()
+                INSTANCE = inst
+                inst
+            }
+    }
+}
