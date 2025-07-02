98ad9806fa204272d6b3bb0c07a9150c62297e2c
Vadim-Li-V <vadker4@gmail.com>
2025-07-02 20:14:32 +0700

    Обновил helpActivity

--- app/src/main/java/com/example/tourguideplus/HelpActivity.kt
+++ app/src/main/java/com/example/tourguideplus/HelpActivity.kt
@@ -2,10 +2,17 @@ package com.example.tourguideplus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tourguideplus.ui.HelpFragment
 
 class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HelpFragment())
+                .commit()
+        }
     }
 }
