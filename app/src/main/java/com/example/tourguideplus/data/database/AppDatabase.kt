75437bfd19d393b1490662aa7877486882e1b85a
Vadim-Li-V <vadker4@gmail.com>
2025-07-02 19:19:58 +0700

    Обновил AppDatabase

--- app/src/main/java/com/example/tourguideplus/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/tourguideplus/data/database/AppDatabase.kt
@@ -6,6 +6,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.tourguideplus.data.dao.PlaceDao
+import com.example.tourguideplus.data.dao.RouteDao
 import com.example.tourguideplus.data.model.Place
 
 @Database(
@@ -15,7 +16,7 @@ import com.example.tourguideplus.data.model.Place
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun placeDao(): PlaceDao
-
+    abstract fun routeDao(): RouteDao
     companion object {
         @Volatile private var INSTANCE: AppDatabase? = null
 
