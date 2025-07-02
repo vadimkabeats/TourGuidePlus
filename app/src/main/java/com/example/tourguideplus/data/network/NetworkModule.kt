022204a104eab3f71ffeebb89ede5600fcebef32
Vadim-Li-V <vadker4@gmail.com>
2025-07-02 14:14:03 +0700

    Добавил разрешение на использование Интернета и добавил network module

---
+++ app/src/main/java/com/example/tourguideplus/data/network/NetworkModule.kt
@@ -0,0 +1,19 @@
+package com.example.tourguideplus.data.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkModule {
+    private const val BASE_URL = "https://api.openweathermap.org/"
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val weatherApi: OpenWeatherApi by lazy {
+        retrofit.create(OpenWeatherApi::class.java)
+    }
+}
